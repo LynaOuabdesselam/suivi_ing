8962b3c3d032c3939141d21620a0e19b8fdf34c6
LynaOuabdesselam <lyna.ouab@gmail.com>
2022-02-23 20:26:30 +0100

    C'est si long

--- telecom2.php
+++ telecom2.php
@@ -14,7 +14,7 @@ class Telecom2 {
     //UE Outils Informatique2
     private $bddTP, $bddCoef, $bdd;
     //private $bddNoteMin, $bddECTS;
-    private $semP, $sem$TP, $semCoef, $sem,
+    private $semP, $semTP, $semCoef, $sem,
     //private $semNoteMin, $semECTS;
     private $ueOutilsInformatiques2, $ueOutilsInformatiques2Reultats;
     //private $ueOutilsInformatiques2NoteMin, $ueOutilsInformatiques2ECTS;
@@ -32,7 +32,7 @@ class Telecom2 {
 
     //UE Traitement du signal2
     private $tnsP , $tnsTP, $tnsCoef, $tns;
-    //private $tnsNoteMin , $tnsECTS;
+    //private $tnsNoteMin , $tnsECTS;$ueTelecom2, $ueTelecom2Resultats
     private $tsaP, $tsaTP ,$tsaCoef, $tsa;
     //private $tsaNoteMin, $tsaECTS ;
     private $ueTraitementSignal2 , $ueOutilsInformatiques2Resultats; 
@@ -41,7 +41,7 @@ class Telecom2 {
     //UE Langues vivante 1
     private $anglaisP , $anglaisCC, $anglaisCoef, $anglais;
     private $ueLanguesVivante1, $ueLanguesVivante1Resultat ;
-    //private $ueLanguesVivante1NoteMin , $$ueLanguesVivante1ECTS ;
+    //private $ueLanguesVivante1NoteMin , $ueLanguesVivante1ECTS ;
 
     //UE Culture d'entreprise 7
     private $introAnalyseDonneesTP , $introAnalyseDonneesCoef, $introAnalyseDonnees ;
@@ -54,7 +54,7 @@ class Telecom2 {
     //private $ueCultureEntrepriseNoteMin, $ueCultureEntrepriseECTS;
 
     //S1 Resultats
-    private $s3Moyenne ,$s3Resultat
+    private $s3Moyenne ,$s3Resultat;
     //private $s3MoyenneNoteMin ,$s3MoyenneECTS;
     /*************************************************************************************************************/
 
@@ -64,14 +64,14 @@ class Telecom2 {
     //UE Outils informatique 3 
     private $javaProjet, $javaTP, $javaCoef, $java;
     //private $javaNoteMin, $javaECTS;
-    private $web2$TP, $web2Coef, $web2,
+    private $web2TP, $web2Coef, $web2,
     //private $web2NoteMin, $web2ECTS;
     private $ueOutilsInformatiques3, $ueOutilsInformatiques3Reultats;
     //private $ueOutilsInformatiques3NoteMin, $ueOutilsInformatiques3ECTS;
 
     //UE Réseaux 2
-    private $modelMarkovP, $$modelMarkovCoef, $$modelMarkov;
-    //private $$modelMarkovNoteMin, $$modelMarkovECTS;
+    private $modelMarkovP, $modelMarkovCoef, $modelMarkov;
+    //private $modelMarkovNoteMin, $modelMarkovECTS;
     private $routageTP, $routageCoef, $routage;
     //private $routageNoteMin , $routageECTS;
     private $rtd2P, $rtd2TP, $rtd2Coef, $rtd2;
@@ -111,7 +111,7 @@ class Telecom2 {
     //private $ouvertureLinguistiqueNoteMin, $ouvertureLinguistiqueECTS;
     private $anglais2; //$ouvertureLinguistique or $anglaisRenforce;
     private $ueLanguesVivante8, $ueLanguesVivanteR8esultat ;
-    //private $ueLanguesVivante8NoteMin , $$ueLanguesVivante8ECTS ;
+    //private $ueLanguesVivante8NoteMin , $ueLanguesVivante8ECTS ;
 
 
     //UE Culture d'entreprise 8
@@ -134,8 +134,175 @@ class Telecom2 {
     private $dettesSpeT2, $remarques;
 
 
-    public function __construct() {
-       
+    public function __construct($nom, $prenom, $idEtudiant, $dettes, $credits,$anneeMoyenne,
+        $anneeResultat, $anneeTOEIC,$TOEICblanc1, $TOEICblanc2, $TOEICofficiel, $anglaisResultats,
+        $bddTP, $bddCoef, $bdd, $semP, $semTP, $semCoef, $sem, $ueOutilsInformatiques2, 
+        $ueOutilsInformatiques2Reultats,$antennesP, $antennesTP, $antennesCoef, $antennes,
+        $theorieInofrmationP, $theorieInofrmationCoef, $theorieInofrmation,$tOptP, $tOptTP,
+         $tOptCoef, $tOpt,$ueTelecom2, $ueTelecom2Resultats,   $tnsP , $tnsTP, $tnsCoef, $tns,
+         $tsaP, $tsaTP ,$tsaCoef, $tsa,$ueTraitementSignal2 , $ueOutilsInformatiques2Resultats,
+        $anglaisP , $anglaisCC, $anglaisCoef, $anglais,$ueLanguesVivante1, 
+        $ueLanguesVivante1Resultat, $introAnalyseDonneesTP ,$introAnalyseDonneesCoef, 
+        $introAnalyseDonnees, $ethiqueP,$ethiqueCoef, $ethique,$introSociologieP , 
+        $introSociologieCoef, $introSociologie,$ueCultureEntreprise, $ueCultureEntrepriseResultat,
+        $s3Moyenne ,$s3Resultat, $javaProjet, $javaTP, $javaCoef, $java,$web2TP, $web2Coef, 
+        $web2, $ueOutilsInformatiques3, $ueOutilsInformatiques3Reultats,$modelMarkovP,
+         $modelMarkovCoef, $modelMarkov,$routageTP, $routageCoef, $routage, $rtd2P, $rtd2TP
+         , $rtd2Coef, $rtd2,$ueReseaux2, $ueReseaux2Resultat, $commNumP1, $commNumP2, $commNumTP
+         , $commNumCoef, $commNum,$fhlsP, $fhlsCoef, $fhls, $artP, $artCoef, $art,$ueTelecom3, 
+         $ueTelecom3Resultats,$cdceP , $cdceTP, $cdceCoef, $cdce,$dspTP ,$dspCoef, $dsp,
+         $ueTraitementSignal3 , $ueOutilsInformatiques3Resultats, $rapportProjet, $exposeProjet, 
+         $TPProjet, $projetThematique, $ueProjetThematique, $ueProjetThematiqueResultat, $anglaisCC,
+          $anglais,$anglaisRenforceCC, $anglaisRenforceCoef, 
+          $anglaisRenforce,$ouvertureLinguistiqueCC, $ouvertureLinguistiqueCoef, 
+          $ouvertureLinguistique, $anglais2,$ueLanguesVivante8, $ueLanguesVivanteR8esultat,
+          $projetCreationEntrepriseCoef , $projetCreationEntrepriseCC, $projetCreationEntreprise, 
+          $qseP, $qseCoef, $qse, $santeSecuriteTravailP , $santeSecuriteTravailCoef, 
+          $santeSecuriteTravail, $ueCultureEntreprise8, $ueCultureEntreprise8Resultat,
+           $s4Moyenne ,$s4Resultat, $dettesSpeT2, $remarques, $commentaires) {
+
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+            $this-->
+
+
+
+
+
+            
+
     }
 
 }
