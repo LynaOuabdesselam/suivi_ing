9b2ab1a467b4c4ce0d9f94c05bdd02b340c54a66
LynaOuabdesselam <lyna.ouab@gmail.com>
2022-02-24 12:11:31 +0100

    readCSV.php

---
+++ readCSV.php
@@ -0,0 +1,28 @@
+<?php
+$csv = array();
+$i = 0;
+if(($handle = fopen("juryT2.csv", "r")) !== FALSE)
+{
+   while(($data = fgetcsv($handle, 1000, "/", "O")) !== FALSE)
+   {
+    $i++; 
+        if($i < 13){
+            continue;
+        }
+        $csv[] = $data;
+        
+   }
+   
+}
+
+// $csv = str_getcsv(file_get_contents('juryT2.csv'));
+
+// echo '<pre>';
+// print_r($csv);
+// echo '</pre>';
+
+echo '<pre>';
+print_r($csv);
+echo '</pre>';
+
+?>
