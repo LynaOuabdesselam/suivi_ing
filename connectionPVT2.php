1f5c7779b9f444d1bfec9e49689531eda220c7e7
LynaOuabdesselam <lyna.ouab@gmail.com>
2022-02-26 17:28:59 +0100

    connection to the db

--- connectionPVT2.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    $con = mysqli_connect('localhost', 'root', '', 'pvT2')
-    if(mysqli_connect_error())
-        echo "Connection Error.";
-    else
-        echo "Database Connection Successfully.";
-?>
